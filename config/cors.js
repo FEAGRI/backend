72053f88504e2647e9d66e05513c0fa18c306d7c
jsfernando <jsfernando@gmail.com>
2017-10-05 14:15:21 -0300

    correcao cors js... header por headers

--- config/cors.js
+++ config/cors.js
@@ -2,6 +2,6 @@
 module.exports = function(request, response, next){
   response.header('Access-Control-Allow-Origin', '*'),
   response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'),
-  response.header('Access-Control-Allow-Header', 'Origin, X-Requested-Width, Content-Type, Accept')
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept')
   next()
 }
